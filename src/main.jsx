730eb5d385ff9ddb8aa18e235af8006aceab51ce
Juniiie <jrbuking@gmail.com>
2023-06-01 02:49:47 +1000

    Completed Navigation section

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import "../node_modules/normalize.css/normalize.css";
+import { BrowserRouter } from "react-router-dom";
+// This will remove default browser CSS
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
