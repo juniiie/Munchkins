44f5d41bac673024145130364da107fa6b58c2cd
Climson Jr <jrbuking@gmail.com>
2023-06-02 00:04:08 +1000

    Working on home page

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: ${(props) => `calc(100vh - ${props.theme.navHeight})`};
+  width: 100vw;
+  position: relative;
+  background-color: ${(props) => props.theme.text};
+`;
+
+const Container = styled.div`
+  width: 75%;
+  min-height: 80vh;
+  margin: 0 auto;
+  background-color: lightblue;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Section>
+      <Container>
+        <Box>
+          <h1>text</h1>
+        </Box>
+        <Box>
+          <h1>Video</h1>
+        </Box>
+      </Container>
+    </Section>
+  );
 };
 
 export default Home;
