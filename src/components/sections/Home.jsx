f7dc6b789e83c0747516e3775a8c97a9fcb23208
Juniiie <jrbuking@gmail.com>
2023-06-06 23:39:56 +1000

    Completed HOME PAGE

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import TypeWriterText from "./TypeWriterText";
+import CoverVideo from "../CoverVideo";
+import RoundTextBlack from "../../assets/Rounded-Text-Black.png";
 
 const Section = styled.section`
   min-height: ${(props) => `calc(100vh - ${props.theme.navHeight})`};
   width: 100vw;
   position: relative;
-  background-color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
 `;
 
 const Container = styled.div`
   width: 75%;
   min-height: 80vh;
   margin: 0 auto;
-  background-color: lightblue;
+  background-color: #ffffff;
 
   display: flex;
   justify-content: center;
@@ -29,6 +31,46 @@ const Box = styled.div`
   align-items: center;
 `;
 
+const rotate = keyframes`
+  100%{
+    transform: rotate(1turn);
+  }
+`;
+
+const Round = styled.div`
+  position: absolute;
+  bottom: 2rem;
+  right: 90%;
+  width: 6rem;
+  height: 6rem;
+  border: 1px solid ${(props) => props.theme.text};
+  border-radius: 50%;
+
+  img {
+    width: 100%;
+    height: auto;
+    animation: ${rotate} 6s linear infinite reverse;
+  }
+`;
+
+const Circle = styled.span`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  font-size: ${(props) => props.theme.fontxl};
+`;
+
 const Home = () => {
   return (
     <Section>
@@ -39,9 +81,13 @@ const Home = () => {
           <TypeWriterText />
         </Box>
         <Box>
-          {/* <h1>Video</h1> */}
-          <img src="../assets/Home GIF.gif" alt="#" />
+          <CoverVideo />
         </Box>
+
+        <Round>
+          <Circle>&#x2193;</Circle>
+          <img src={RoundTextBlack} alt="NFT" />
+        </Round>
       </Container>
     </Section>
   );
--- src/components/sections/TypeWriterText.jsx
+++ src/components/sections/TypeWriterText.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import styled from "styled-components";
+import Button from "../Button";
 
 const Title = styled.h2`
   font-size: ${(props) => props.theme.fontxxxl};
@@ -13,29 +14,59 @@ const Title = styled.h2`
     text-transform: uppercase;
     font-family: "Akaya Telivigala", cursive;
   }
+  .text-1 {
+    color: blue;
+  }
+  .text-2 {
+    color: orange;
+  }
+  .text-3 {
+    color: red;
+  }
+`;
+
+const SubTitle = styled.h3`
+  font-size: ${(props) => props.theme.fontlg};
+  text-transform: capitalize;
+  color: ${(props) => `rgba(${props.theme.text}, 0.6)`};
+  font-weight: 600;
+  width: 80%;
+  align-self: flex-start;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 10px;
+  width: 80%;
+  align-self: flex-start;
 `;
 
 const TypeWriterText = () => {
   return (
-    <Title>
-      Discover a new era of cool
-      <Typewriter
-        options={{ autoStart: true, loop: true }}
-        onInit={(typewriter) => {
-          typewriter
-            .typeString("<span class='text-1'>NFTS.</span>")
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString("<span class='text-1'>Collectible Items.</span>")
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString("<span class='text-1'>Ape Killers!</span>")
-            .pauseFor(2000)
-            .deleteAll()
-            .start();
-        }}
-      />
-    </Title>
+    <>
+      <Title>
+        Discover a new era of cool
+        <Typewriter
+          options={{ autoStart: true, loop: true }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("<span class='text-1'>NFTS.</span>")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("<span class='text-2'>Collectible Items.</span>")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("<span class='text-3'>Ape Killers!</span>")
+              .pauseFor(2000)
+              .deleteAll()
+              .start();
+          }}
+        />
+      </Title>
+      <SubTitle>Bored of Apes? Try Something New.</SubTitle>
+      <ButtonContainer>
+        <Button text="Explore" link="#about" />
+      </ButtonContainer>
+    </>
   );
 };
 
