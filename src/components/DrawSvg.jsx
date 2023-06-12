6de98fc2d8460b3d6a5c45f14582afbfb5675fa6
Juniiie <jrbuking@gmail.com>
2023-06-12 18:51:16 +1000

    DrawSVG component complete

--- src/components/DrawSvg.jsx
+++ src/components/DrawSvg.jsx
@@ -1,8 +1,8 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import styled from "styled-components";
 import Vector from "../Icons/Vector";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const VectorContainer = styled.div`
   position: absolute;
@@ -29,13 +29,34 @@ const DrawSvg = () => {
 
     let svg = document.getElementsByClassName("svg-path")[0];
 
-    const length = svg.getBoundingClientRect();
-
+    const length = svg.getTotalLength();
     console.log(length);
+
+    // Start positioning of svg drawing
+    svg.style.strokeDasharray = length;
+
+    // Hide svg before scrolling starts
+    svg.style.strokeDashoffset = length;
+
+    let t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        start: "top center",
+        end: "bottom bottom",
+        onUpdate: (self) => {
+          const draw = length * self.progress;
+          // console.log(self);
+
+          // Also reverse the drawing when scroll goes up
+          svg.style.strokeDashoffset = length - draw;
+        },
+      },
+    });
+
     return () => {};
   }, []);
   return (
-    <VectorContainer>
+    <VectorContainer ref={ref}>
       <Vector />
     </VectorContainer>
   );
